261000f29ce529461ba89ee13464bdfef6cf49c3
chris-malloy <chrismalloymusic@gmail.com>
2017-12-07 10:32:28 -0500

    added header auth, frontend now console.logs student array

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/getStudents', function(req, res, next) {
+  	const students = [
+		"Eddie",
+		"Valerie",
+		"Michael",
+		"Scott",
+		"Jennifer",
+		"Nikolas",
+		"Zach",
+		"Casey",
+		"Jamie",
+		"Amir",
+		"Akil",
+		"Binh",
+		"Jong",
+		"Jason",
+		"Chris",
+		"Saif",
+		"Taylor"
+	  ];
+	  res.json(students);
 });
 
 module.exports = router;
