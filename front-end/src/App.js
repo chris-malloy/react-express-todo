e4166e4014b112fd06243bb7f1d876cf6c74e36e
chris-malloy <chrismalloymusic@gmail.com>
2017-12-07 12:01:05 -0500

    added post route for addStudent

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,23 +4,55 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			students: [],
+		}
+	}
 
 	componentDidMount(){
 		axios.get('http://localhost:3000/getStudents')
-		.then((response)=>{
-			console.log(response)
+			.then((response)=>{
+				console.log(response)
+				this.setState({
+					students: response.data,
+				})
 		})
 	}
+
+	handleSubmit(event){
+		event.preventDefault();
+		const studentName = document.getElementById('new-student').value;
+		axios({
+			method: "POST",
+			url: "http://localhost:3000/addStudent",
+			// express will have accesss to this data object
+			data: {
+				studentName: studentName
+			}
+		}).then((data)=>{
+			console.log(data)
+		})
+		
+	}
 	render() {
+		console.log(this.state)
+		var studentsArray = this.state.students.map((student,i)=>{
+			return(<li key={i}>{student.name}</li>)
+		})
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				<div>
+					<h1>Todo</h1>
+				</div>
+				<form onSubmit={this.handleSubmit}>
+					<input type="text" id="new-student" placeholder="New Student Name"/>
+					<button type="submit">Add Student</button>
+				</form>
+				<div>
+					{studentsArray}
+				</div>
 			</div>
 		);
 	}
